dbb33516a4541c83df289e482fc9ac266b15055f
ToanNguyenn <88270091+ToanNguyenn@users.noreply.github.com>
2022-11-27 20:08:32 +0700

    Init repo

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import time
+app = Flask(__name__)
+@app.route('/')
+
+def hello_world():
+    return f"Hello World at {time.time()}"
+
+if __name__ == '__main__':
+    print("App run!!")
+    app.run(debug=True)
